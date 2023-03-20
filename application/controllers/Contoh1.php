a81e48590857d32cb8c6f6ac4af0ded68c2367c7
melvinsalwa <melvinesalwa13@gmail.com>
2023-03-20 16:25:08 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+public function index()
+{
+echo "<h1>Perkenalkan</h1>";
+echo"Nama saya Melvine Salwa Dewanto
+Saya tingga di daerah Rawa Buaya
+olah raga yang saya sukai adalah
+Bulutangkis";
+
+}
+}
